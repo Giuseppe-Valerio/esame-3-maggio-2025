5789af1b7e507130d3c0393830bb8f5880b16e6b
Giuseppe-Valerio <giuseppevalerio8989@gmail.com>
2025-05-04 15:09:34 +0200

    Initial commit

---
+++ src/main/java/it/epicode/esame_3_maggio_2025/Esame3Maggio2025Application.java
@@ -0,0 +1,13 @@
+package it.epicode.esame_3_maggio_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esame3Maggio2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esame3Maggio2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/esame_3_maggio_2025/Esame3Maggio2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.esame_3_maggio_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esame3Maggio2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
